56eef2742436c109c4f3611e6cff4f689c2fe9e3
Jeremy Tan <tan_kai_qun97@yahoo.com.sg>
2022-06-13 15:35:27 +0800

    Fix hydration error

--- frontend/src/components/safe-hydrate.tsx
+++ frontend/src/components/safe-hydrate.tsx
@@ -1,15 +1,19 @@
-import { ReactNode } from "react";
+import { useState, ReactNode, useEffect } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
 function SafeHydrate({ children }: Props) {
-  return (
-    <div suppressHydrationWarning>
-      {typeof window === "undefined" ? null : children}
-    </div>
-  );
+  // TODO: fix this next time
+  // previously, it was typeof window === "undefined"
+  const [isSSR, setSSR] = useState(true);
+
+  useEffect(() => {
+    setSSR(false);
+  }, []);
+
+  return <div suppressHydrationWarning>{isSSR ? null : children}</div>;
 }
 
 export default SafeHydrate;

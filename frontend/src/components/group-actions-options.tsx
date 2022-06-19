ff015a9f071978a31bbc5619447f003505800d10
Cynthia <leewengyancynthia@gmail.com>
2022-06-20 02:06:58 +0800

    Refactor rename group option

---
+++ frontend/src/components/group-actions-options.tsx
@@ -0,0 +1,111 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Menu } from "@mantine/core";
+import { useModals } from "@mantine/modals";
+import { FormProvider, useForm } from "react-hook-form";
+import { FaEdit } from "react-icons/fa";
+import { z } from "zod";
+import { NAME } from "../constants";
+
+import { useRenameCourseGroupMutation } from "../redux/services/groups-api";
+import { Course } from "../types/courses";
+import { GroupPatchAction, GroupSummaryView } from "../types/groups";
+import { useResolveError } from "../utils/error-utils";
+import { handleSubmitForm } from "../utils/form-utils";
+import toastUtils from "../utils/toast-utils";
+import TextField from "./text-field";
+
+type Props = {
+  course?: Course;
+  group?: GroupSummaryView;
+  hasAdminPermission?: boolean;
+  userIsInGroup?: boolean;
+};
+
+type OptionProps = {
+  hidden: boolean;
+  course?: Course;
+  group?: GroupSummaryView;
+};
+
+const schema = z.object({
+  [NAME]: z.string().trim().min(1, "Please enter a group name"),
+});
+
+type CourseGroupRenameProps = z.infer<typeof schema>;
+
+function RenameGroupOption({ hidden, group, course }: OptionProps) {
+  const modals = useModals();
+  const [renameGroup, { isLoading }] = useRenameCourseGroupMutation();
+
+  const methods = useForm<CourseGroupRenameProps>({
+    resolver: zodResolver(schema),
+    defaultValues: { [NAME]: `${group ? group?.name : ""}` },
+  });
+  const resolveError = useResolveError();
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onRenameCourseGroup = async (formData: CourseGroupRenameProps) => {
+    const groupId = group?.id;
+    const courseId = course?.id;
+
+    console.log("onRenameCourseGroup");
+    if (
+      courseId === undefined ||
+      groupId === undefined ||
+      isLoading ||
+      isSubmitting
+    ) {
+      return;
+    }
+
+    console.log("send API req");
+
+    const renameData = {
+      action: GroupPatchAction.Modify,
+      payload: {
+        name: formData[NAME],
+      },
+    };
+
+    await renameGroup({ ...renameData, courseId, groupId }).unwrap();
+
+    toastUtils.success({
+      message: "Successfully renamed group",
+    });
+  };
+
+  // TODO: Fix this issue
+  const openRenameGroupModal = () =>
+    modals.openConfirmModal({
+      title: "Rename group",
+      closeButtonLabel: "Cancel renaming group",
+      centered: true,
+      children: (
+        <FormProvider {...methods}>
+          <TextField name={NAME} />
+        </FormProvider>
+      ),
+      labels: { confirm: "Save changes", cancel: "Cancel" },
+      confirmProps: { color: "green", loading: isLoading },
+      onConfirm: () => {
+        console.log("SUBMIT");
+        handleSubmitForm(handleSubmit(onRenameCourseGroup), resolveError);
+      },
+    });
+
+  return (
+    <Menu.Item
+      icon={<FaEdit size={14} />}
+      hidden={hidden}
+      onClick={openRenameGroupModal}
+    >
+      Rename group
+    </Menu.Item>
+  );
+}
+
+export default RenameGroupOption;
--- frontend/src/components/group-card-actions-menu.tsx
+++ frontend/src/components/group-card-actions-menu.tsx
@@ -17,96 +17,18 @@ import { GroupPatchAction, GroupSummaryView } from "../types/groups";
 import { useResolveError } from "../utils/error-utils";
 import { handleSubmitForm } from "../utils/form-utils";
 import toastUtils from "../utils/toast-utils";
+import RenameGroupOption from "./group-actions-options";
 import TextField from "./text-field";
 
 type Props = {
   course?: Course;
-  courseId?: number | string;
   group?: GroupSummaryView;
   hasAdminPermission?: boolean;
   userIsInGroup?: boolean;
 };
 
-type OptionProps = {
-  hidden: boolean;
-  courseId?: number | string;
-  group?: GroupSummaryView;
-};
-
-const schema = z.object({
-  [NAME]: z.string().trim().min(1, "Please enter a group name"),
-});
-
-type CourseGroupRenameProps = z.infer<typeof schema>;
-
-function RenameGroupOption({ hidden, group, courseId }: OptionProps) {
-  const modals = useModals();
-  const [renameGroup, { isLoading }] = useRenameCourseGroupMutation();
-
-  const methods = useForm<CourseGroupRenameProps>({
-    resolver: zodResolver(schema),
-    defaultValues: { [NAME]: `${group ? group?.name : ""}` },
-  });
-  const resolveError = useResolveError();
-
-  const {
-    handleSubmit,
-    formState: { isSubmitting },
-  } = methods;
-
-  const onRenameCourseGroup = async (formData: CourseGroupRenameProps) => {
-    if (
-      courseId === undefined ||
-      group?.id === undefined ||
-      isLoading ||
-      isSubmitting
-    ) {
-      return;
-    }
-    const groupId = group?.id;
-
-    const renameData = {
-      action: GroupPatchAction.Modify,
-      payload: {
-        name: formData[NAME],
-      },
-    };
-
-    await renameGroup({ ...renameData, courseId, groupId }).unwrap();
-  };
-
-  const openRenameGroupModal = () =>
-    modals.openConfirmModal({
-      title: "Rename group",
-      closeButtonLabel: "Cancel renaming group",
-      centered: true,
-      children: (
-        <FormProvider {...methods}>
-          <TextField name={NAME} />
-        </FormProvider>
-      ),
-      labels: { confirm: "Save changes", cancel: "Cancel" },
-      confirmProps: { color: "green", loading: isLoading },
-      onConfirm: () => {
-        console.log("SUBMIT");
-        handleSubmitForm(handleSubmit(onRenameCourseGroup), resolveError);
-      },
-    });
-
-  return (
-    <Menu.Item
-      icon={<FaEdit size={14} />}
-      hidden={hidden}
-      onClick={openRenameGroupModal}
-    >
-      Rename group
-    </Menu.Item>
-  );
-}
-
 function GroupCardActionsMenu({
   course,
-  courseId,
   group,
   hasAdminPermission,
   userIsInGroup,
@@ -117,6 +39,8 @@ function GroupCardActionsMenu({
     useDeleteCourseGroupMutation();
 
   const modals = useModals();
+  const courseId = course?.id;
+  const groupId = group?.id;
 
   const canJoinGroup =
     !userIsInGroup && (hasAdminPermission || course?.allowStudentsToJoinGroups);
@@ -147,12 +71,10 @@ function GroupCardActionsMenu({
     if (
       isJoiningOrLeavingGroup ||
       courseId === undefined ||
-      group?.id === undefined
+      groupId === undefined
     ) {
       return;
     }
-
-    const groupId = group?.id;
 
     const groupPutData = {
       action,
@@ -162,18 +84,15 @@ function GroupCardActionsMenu({
     };
     await joinOrLeaveGroup({ ...groupPutData, courseId, groupId }).unwrap();
 
-    // TODO: update message for diff actions and handle error cases
     toastUtils.success({
       message: `The group has been updated successfully.`,
     });
-    // onSuccess?.();
   };
 
   const onDeleteCourseGroup = async () => {
-    if (courseId === undefined || group?.id === undefined || isDeletingGroup) {
+    if (courseId === undefined || groupId === undefined || isDeletingGroup) {
       return;
     }
-    const groupId = group?.id;
 
     await deleteGroup({ courseId, groupId }).unwrap();
 
@@ -181,8 +100,6 @@ function GroupCardActionsMenu({
       message: `The group has been successfully deleted.`,
     });
   };
-
-  // TODO: refactor these openModal commands?
 
   const openDeleteGroupModal = () =>
     modals.openConfirmModal({
@@ -259,7 +176,7 @@ function GroupCardActionsMenu({
     >
       <RenameGroupOption
         hidden={!canModifyGroupName}
-        courseId={courseId}
+        course={course}
         group={group}
       />
       <Menu.Item

bda0923602f65f2852d00a9cfacc12b6e1fc5a70
Jeremy Tan <jeremytan@stripe.com>
2022-08-16 23:37:52 +0800

    Allow inner scroll on main view

--- frontend/src/components/app-layout.tsx
+++ frontend/src/components/app-layout.tsx
@@ -24,6 +24,7 @@ const useStyles = createStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     flex: "1 1 auto",
+    overflow: "hidden",
   },
   contentContainer: {
     display: "flex",
